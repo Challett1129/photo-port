df185c30989ef434c80fa777a1f215298b57cdfa
Collin <collinhallett@gmail.com>
2021-11-01 01:45:37 -0500

    added flex-row class to textarea in contact component to fix spacing error

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -50,7 +50,7 @@ function ContactForm() {
             </div>
             <div>
                 <label htmlFor="message">Message:</label>
-                <textarea name="message" rows="5" defaultValue={message} onBlur={handleChange} />
+                <textarea name="message" className="flex-row" rows="5" defaultValue={message} onBlur={handleChange} />
             </div>
             {errorMessage && (
                 <div>
